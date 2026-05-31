17c6a0b04afb4edbbbff11b1e247ffef87f29d55
kazari4 <skidyuk86115@gmail.com>
2026-05-31 11:02:10 +0900

    fix/フォーマットを修正

--- app/_components/CongestionCard.tsx
+++ app/_components/CongestionCard.tsx
@@ -3,10 +3,7 @@ import { Card } from "./ui/Card";
 
 export function CongestionCard() {
   return (
-    <Card
-      icon={<Users className="h-5 w-5" />}
-      title="学内MAP"
-    >
+    <Card icon={<Users className="h-5 w-5" />} title="学内MAP">
       <div className="relative h-56 overflow-hidden rounded-2xl bg-[url('/images/map_image_no_numbers.png')] bg-contain bg-center bg-no-repeat sm:h-72" />
     </Card>
   );
