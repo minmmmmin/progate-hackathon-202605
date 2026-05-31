d5421e21ae9f0bc83fd88a873e0bb7eb3cbe54e0
kazari4 <skidyuk86115@gmail.com>
2026-05-31 11:00:00 +0900

    数字が入ってない画像に変更

--- app/_components/CongestionCard.tsx
+++ app/_components/CongestionCard.tsx
@@ -7,7 +7,7 @@ export function CongestionCard() {
       icon={<Users className="h-5 w-5" />}
       title="学内MAP"
     >
-      <div className="relative h-56 overflow-hidden rounded-2xl bg-[url('/images/map_image.png')] bg-contain bg-center bg-no-repeat sm:h-72" />
+      <div className="relative h-56 overflow-hidden rounded-2xl bg-[url('/images/map_image_no_numbers.png')] bg-contain bg-center bg-no-repeat sm:h-72" />
     </Card>
   );
 }
