3229c0791a104451b68a500641c6026105561d4a
mitchi <pg.saku0719@gmail.com>
2026-05-31 15:01:08 +0900

    画像差し替え

--- app/_components/CongestionCard.tsx
+++ app/_components/CongestionCard.tsx
@@ -4,7 +4,7 @@ import { Card } from "./ui/Card";
 export function CongestionCard() {
   return (
     <Card icon={<Users className="h-5 w-5" />} title="学内MAP">
-      <div className="relative h-56 overflow-hidden rounded-2xl bg-[url('/images/map_image_no_numbers.png')] bg-contain bg-center bg-no-repeat sm:h-72" />
+      <div className="relative h-56 overflow-hidden rounded-2xl bg-[url('/images/map.png')] bg-contain bg-center bg-no-repeat sm:h-72" />
     </Card>
   );
 }
